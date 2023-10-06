d6a86622c8ce685d10ee4ebda7c20e91a17a639f
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-06 19:35:47 +0300

    test001

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int space;
+	int comma;
+
+	i = 48;
+	space = 32;
+	comma = 44;
+	while (i < 58)
+	{
+		j = i + 1;
+		while (j < 58 && j > i)
+		{
+			putchar((char)i);
+			putchar((char)j);
+			if (i != 56 && j != 57)
+			{
+				putchar((char)comma);
+				putchar((char)space);
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
