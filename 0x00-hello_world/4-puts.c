e81741b4eeb331bc037bf2ea418b031365774343
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-06 04:28:22 +0300

    test05

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#include<stdio.h>
-/*
-*this code is top print without printf 
-*/
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
