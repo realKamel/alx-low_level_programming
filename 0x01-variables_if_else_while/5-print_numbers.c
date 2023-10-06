fc1c8b919868072a1024b6dce360dc2d088ffe0b
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-06 15:05:54 +0300

    test001

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+
+	printf("0");
+	while (i < 10)
+		printf("i", i++);
+	return (0);
+}
