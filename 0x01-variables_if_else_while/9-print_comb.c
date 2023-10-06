71e0502883fafbd34f27f184a2d703db17d0df82
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-06 16:35:26 +0300

    test001

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int comma;
+	int space;
+
+	i = 48;
+	comma = 44;
+	space = 32;
+	while (i < 58)
+	{
+		putchar((char)i);
+		if (i != 57)
+		{
+			putchar((char)comma);
+			putchar((char)space);
+		}
+		i++;
+	}
+	return (0);
+}
