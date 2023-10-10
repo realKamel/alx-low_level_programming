22179faaa6c9ee324366520eff0ee5783a2a842f
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-11 00:10:57 +0300

    test001

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_islower - perform a check if the char is lowecase or not
+ * @c : the char to check
+ * Return: 1 if it belongs to alphapets
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
