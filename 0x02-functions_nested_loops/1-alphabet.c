f7e3bdd5450456b09f1665c6a585594c4b4b7d60
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-10 23:35:15 +0300

    test003

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,13 @@
 #include "main.h"
+/**
+ * print_alphabet - print the alphapets
+ *
+ * Return: it return void
+*/
 void print_alphabet(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
