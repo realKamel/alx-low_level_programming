c50dfbdc7ac9650d85982ffbc044bb0b02af266b
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-11 16:47:44 +0300

    test01

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the abslute value of number 
+ * @n : takes the number to computes it
+ * Return: it the return the abs value of int
+*/
+int _abs(int n)
+{
+	if (n > 0)
+		reurn (n);
+	else if (n < 0)
+		return (-n);
+	else
+		return (0);
+}
