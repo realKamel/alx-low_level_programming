0a341f9c889bec06c357f11dd189366d6c4dcf63
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-12 22:39:06 +0300

    tst1

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - The numbers speak for themselves
+ *
+ * Return: no return value
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+}
