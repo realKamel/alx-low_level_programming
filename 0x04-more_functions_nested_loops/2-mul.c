bdff5c045235bd4f0df2ff41efd8a993225e02bd
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-12 22:33:49 +0300

    tst1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - mul the two paramter the passed to it
+ * @a : the first parameter
+ * @b : the second parameter
+ *
+ * Return: 1 if is digit 0 otherwise 0
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
