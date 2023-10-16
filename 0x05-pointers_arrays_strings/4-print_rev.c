b011c84d7740180e364f7b82bf1fc9698c7d054b
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-17 00:18:02 +0300

    printing array in rev oreder

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - print char array in reverse order
+ * @s: pointer to the index 0 of char array
+ * Return:void
+*/
+void print_rev(char *s)
+{
+	char *runner = s;
+	int i = 0;
+
+	for ( ; (*runner) != '\0'; runner++)
+		i++;
+	for ( ; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
