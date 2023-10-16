a4f6f17b50b5282544619faf643fe5ec4a71334a
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-16 22:35:08 +0300

    test1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap two intgers with pointer
+ * @a : pointer to the first parameter
+ * @b : pointer to the second parameter
+ *
+ * Return: void
+*/
+void swap_int(int *a, int *b)
+{
+	int *tmp = 0;
+
+	tmp = a;
+	a = b;
+	b = a;
+}
