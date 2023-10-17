673e10df21ed1e428244a6dea2478c4d13395715
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-17 20:52:48 +0300

    printing string in rev oreder_ fix typos

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string.
+ * @s : pointer to char array
+ * Return: void
+*/
+void rev_string(char *s)
+{
+	int i,size = 0;
+	char tmp;
+
+	for (i = 0; s[i] != '\0'; i++)
+		size++;
+	for (i = 0; i < (size/2) ; i++)
+	{
+		tmp = s[i];
+		s[i] = s[size-i-1];
+		s[size-i-1] = tmp;
+	}
+}
