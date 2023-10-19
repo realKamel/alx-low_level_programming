d67348a74a6cfa4496d0e1e5fcccf8e611114faa
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-19 14:53:22 +0300

    function to copy strings with range

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a strig
+ * @src: pointer to the source char array
+ * @dest: pointer to the destnation array
+ * @n: the amount of chars to copy
+ * Return: pointer to the dest char array afer catenate
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
