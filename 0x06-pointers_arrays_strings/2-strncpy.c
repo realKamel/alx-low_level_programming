218f7bcd135248e394f95ae7f2c3c65bf113ef93
Abdelrahman <abdo.aly.k.m@outlook.com>
2023-10-19 16:32:10 +0300

    function to copy strings with range

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,11 +9,13 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, src_size;
+	int i;
 
-	for (src_size = 0; src[src_size] != '\0'; src_size++)
-		;
-	for (i = 0; i < n && i <= src_size; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
+
+	for (; n > i; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
