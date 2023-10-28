2182683876158928379ae49ab5572c4c82e2d855
Abderlrahman <abdo.aly.k.m@gmail.com>
2023-10-28 21:10:29 +0200

    simple use of arg of main

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ * @argc: count of arguments
+ * @argv: array of char pointer
+ *
+ * Return: 0 if success
+*/
+int main(__attribute__((unused))int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+#what is argc,argc
