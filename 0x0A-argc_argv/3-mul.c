1dde48508fb7934889ea08c110c5c1335b88acd2
Abderlrahman <abdo.aly.k.m@gmail.com>
2023-10-28 22:37:46 +0200

    simple use of argc and argvof main2

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,16 +7,16 @@
  *
  * Return: 0 if success
 */
-int main(int argc, char *argv[])
+int main(__attribute__((unused))int argc, char *argv[])
 {
-	if (argc == 1)
+	if (argv[1] && argv[2])
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-	}
-	return (0);
 }
